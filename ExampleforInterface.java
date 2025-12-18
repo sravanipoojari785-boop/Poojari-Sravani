fa9ac01fdc4aec406d4392b41ab300eecb58397b
sravanipoojari785-boop <sravanipoojari785@gmail.com>
2025-12-18 20:28:13 +0530

    ExampleforInterface.java

---
+++ ExampleforInterface.java
@@ -0,0 +1,23 @@
+interfacenterface Calculator{
+     void add();
+    void sub();
+}
+class MyCalculator1 implements Calculator{
+    public void add(){
+            int number1 = 20;
+            int number2 = 30;
+            int sum = number1+number2;
+            System.out.println("The sum is:"+ sum);
+        }
+       public  void sub(){
+            int number1 = 20;
+            int number2 = 30;
+            int diff = number1-number2;
+            System.out.println("The difference is:"+ diff);
+        }
+}
+class MyCalculator
+
+    public class ExampleforInterface{}
+
+Calculator.java
