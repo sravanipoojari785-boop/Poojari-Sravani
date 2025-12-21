1d2eb1aa6f596e4804dd6f8800770c0124de81c8
sravanipoojari785-boop <sravanipoojari785@gmail.com>
2025-12-21 14:12:16 +0530

    ExampleForString

---
+++ ExampleForString.java
@@ -0,0 +1,17 @@
+public class ExampleForString {
+    public static void main(String[] args){
+       /*  String s = new String("Vanisha");
+        System.out.println(s);
+        s.concat("Nageena");
+        System.out.println(s);*/
+        /*StringBuffer sb = new StringBuffer("Vanish");
+        System.out.println(sb);
+        sb.append("Nageena");
+        System.out.println(sb);/* */
+        StringBuilder sb = new StringBuilder("Vanish");
+        System.out.println(sb);
+        sb.append("Nageena");
+        System.out.println(sb);
+    }
+    
+}
